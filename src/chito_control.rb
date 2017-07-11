d69e9086eee66e7855d1fdb47785bdde93c694f5
Roddy González <roddy.gonzalez.89@gmail.com>
2017-07-10 22:32:11 -0400

    Refactorized the way commands are called
    Added a class called "ChitoControl". It will recieve and handle "message" object,
    and start a thread to call "ChitoCmd" methods.

---
+++ src/chito_control.rb
@@ -0,0 +1,94 @@
+require_relative "chito_cmd"
+require "thread"
+
+
+class ChitoControl
+
+    #This is the dictionary used to know where to call when a command arrives
+    #In the left: the command string in the incoming text
+    #In the right: the name of the function to be called
+    $commands={
+        '/echo'=>    "echo",
+        '/start'=>   "greet",
+        '/greet'=>   "greet",
+        '/roll'=>    "roll",
+        '/9gag'=>    "nine_gag",
+        '/viernes'=> "viernes",
+        '/help'=>    "help"
+    }
+
+    # Given a command, it returns the first word
+    # If input is not a string, it returns its input
+    def self.get_command(text)
+        if (text.is_a? String) and (text[0] == '/')
+            first_word = text.split(' ')[0]
+            return first_word
+        else
+            return nil
+        end
+    end
+
+    # Given a command name, execute the proper function
+    def self.exec_command(message, bot, log)
+        log.info("IN  - channel:#{message.chat.id} text:\"#{message.text}\"")
+        command = get_command(message.text)
+        if command != nil
+            method = $commands[command] 
+            log.debug("command:#{command} method:#{method.inspect}")
+            if self.methods().include?(:"#{method}")
+                Thread.new do
+                    begin
+                        message.reply do |reply|
+                        reply.text = send(method, message)
+                        reply.send_with(bot)
+                        log.info("OUT - channel:#{message.chat.id} text:\"#{reply.text}\"")
+                        end
+                    rescue Exception => e
+                        log.warn("command:#{command} exception:#{e.message}")
+                    end
+                end
+            else
+                log.debug("method #{method.inspect} not found")
+            end
+        end
+    end
+
+    #######################################################################
+    # Here, you put commands implementation
+    # Normally a command have a method here, a method in ChitoCmd module
+    # and, if needed, a methond in ChitoUtils
+    # Here you handle the message object
+    # In ChitoCmd you handle the calculus and external services
+    # In ChitoUtils you may put some usefull methods
+    #######################################################################
+    
+    # Echo command. Return the given input
+    def self.echo(message)
+        return ChitoCmd.echo(message.text)
+    end
+
+    # Greet command. Salute the user
+    def self.greet(message)
+        return ChitoCmd.greet(message.user.first_name)
+    end
+
+    # 9gag command. Return a random 9gag post from its homepage
+    def self.nine_gag(message)
+        return ChitoCmd.nine_gag()
+    end
+
+    # Roll command. Roll a dice or a group of dices
+    def self.roll(message)
+        return ChitoCmd.roll(message.text)
+    end
+
+    # Sing the Viernes song
+    def self.viernes(message)
+        return ChitoCmd.viernes()
+    end
+
+    # Print help text
+    def self.help(message)
+        return ChitoCmd.help()
+    end
+end
--- src/main.rb
+++ src/main.rb
@@ -1,6 +1,6 @@
 require 'telegram_bot'
 require 'logger'
-require_relative 'chito_cmd'
+require_relative 'chito_control'
 
 def main(telegram_token)
     $LOG = Logger.new("ChitoBot.log", "monthly")
@@ -12,41 +12,12 @@ end
 def get_updates(bot)
     begin
         bot.get_updates(fail_silently: true) do |message|
-            command = message.get_command_for(bot)
-            call_cmd(command, message, bot)
+            ChitoControl.exec_command(message, bot, $LOG)
         end
     rescue Excon::Error::Timeout
         $LOG.warn("get_updates() - time out!")
         sleep 1
         get_updates(bot)
-    end
-end
-
-def call_cmd(command, message, bot)
-    cmd = ChitoCmd.get_first_word(command)
-    $LOG.info("IN  - channel:#{message.chat.id} text:\"#{message.text}\"")
-    message.reply do |reply|
-        must_reply = true
-        case cmd
-        when "echo"
-            reply.text = ChitoCmd.echo(message.text)
-        when "greet", "start"
-            reply.text = ChitoCmd.greet(message.from.first_name)
-        when "9gag"
-            reply.text = ChitoCmd.nine_gag()
-        when "roll"
-            reply.text = ChitoCmd.roll(message.text)
-        when "viernes"
-            reply.text = ChitoCmd.viernes()
-        when "help"
-            reply.text = ChitoCmd.help()
-        else
-            must_reply = false
-        end
-        if must_reply
-            reply.send_with(bot)
-            $LOG.info("OUT - channel:#{message.chat.id} text:\"#{reply.text.inspect}\"")
-        end
     end
 end
 
